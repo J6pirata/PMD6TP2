e5a3d58b4c1bb61ef809135585b33bbbd3b31eda
J6pirata <74542025+J6pirata@users.noreply.github.com>
2023-09-22 01:12:11 -0300

    Add files via upload

---
+++ App.xaml.cs
@@ -0,0 +1,12 @@
+﻿namespace TarefasAPP
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new AppShell();
+        }
+    }
+}
---
+++ MainPage.xaml.cs
@@ -0,0 +1,24 @@
+﻿namespace TarefasAPP
+{
+    public partial class MainPage : ContentPage
+    {
+
+
+        public MainPage()
+        {
+            InitializeComponent();
+
+            List<Tarefas> tarefas = new List<Tarefas>()
+            {
+                new Tarefas { Name = "Tirar o Lixo", Descrition = " Jogar Lixo fora a noite " },
+                new Tarefas { Name = "Alimentar os bichos", Descrition = " Alimente os bichos antes de dormi " },
+                new Tarefas { Name = "Escovar os dentes", Descrition = " Escove os dentes 3 vezes ao dia " },
+            };
+            listTarefas.ItemsSource = tarefas;
+        }
+        public class Tarefas
+        {
+            public string Name { get; set; }
+            public String Descrition { get; set; }
+        } 
+}
